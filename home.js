0eb6905a362751bb650c62f2d6e1f178bbeea806
AATG-ui <76041889+AATG-ui@users.noreply.github.com>
2021-03-12 12:21:16 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,19 @@
+function lounge()
+{
+    window.location = "lounge.html";
+}
+
+function food()
+{
+    window.location = "food.html";
+}
+
+function planes()
+{
+    window.location = "planes.html";
+}
+
+function destinations()
+{
+    window.location = "destinations.html";
+}
